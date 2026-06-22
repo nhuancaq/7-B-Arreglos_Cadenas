fceeed30a128086ece673b3ccdf302cb6f1e68cf
nhuancaq <natalia.lizethqq@gmail.com>
2026-06-22 06:58:09 -0600

    EjercicioF: Ultimo intento (Reemplazar las letras manteniendo simbolos y espacios)

---
+++ EjercicioF5.3.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string texto;
+
+    getline(cin, texto);
+
+    string letras = "";
+
+    for (int i = 0; i < texto.length(); i++) {
+        if (texto[i] >= 'a' && texto[i] <= 'z') {
+            letras += texto[i];
+        }
+    }
+
+    for (int i = 0; i < letras.length() - 1; i++) {
+        for (int j = 0; j < letras.length() - i - 1; j++) {
+            if (letras[j] > letras[j + 1]) {
+                char aux = letras[j];
+                letras[j] = letras[j + 1];
+                letras[j + 1] = aux;
+            }
+        }
+    }
+
+    int pos = 0;
+
+    for (int i = 0; i < texto.length(); i++) {
+        if (texto[i] >= 'a' && texto[i] <= 'z') {
+            texto[i] = letras[pos];
+            pos++;
+        }
+    }
+
+    cout << texto;
+
+    return 0;
+}
