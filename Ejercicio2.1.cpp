b6d77e56af6d074c389dc4ca0fe924ffe5edf9cd
nhuancaq <natalia.lizethqq@gmail.com>
2026-06-23 09:11:39 -0600

    Ejercicio2:Se agrego ciclos for para imprimir todos los pares e impares

---
+++ Ejercicio2.1.cpp
@@ -0,0 +1,49 @@
+//Ejercicio 2 [Práctica de arreglos unidimensionales]
+//Desarrolle un programa de n elementos y forme dos arreglos nuevos,uno con los elementos pares y otro con los elemtos impares del arreglo
+#include <iostream>
+using namespace std;
+int main (){
+
+    int n;
+    cout<<"Ingrese la cantidad de los elementos del arreglo:"<<endl;
+    cin>>n;
+    int A[n];
+    int pares[n];
+    int impares[n];
+    int contPares = 0;
+    int contImpar = 0;
+
+    cout<<"Ingrese los numeros:"<<endl;
+    for(int i=0;i<n;i++){
+        cin>>A[i];
+
+    }
+    for(int i=0;i<n;i++){
+        if(A[i]%2==0){
+            pares[contPares]=A[i];
+            contPares++;
+
+        }else{
+            impares[contImpar]=A[i];
+            contImpar++;
+
+            
+        }
+
+        
+    }
+    cout<<"  Arreglo de numeros pares: ";
+    for(int i=0;i<contPares;i++){
+        cout<<pares[i]<<" ";
+    }
+    cout<<"  Arreglo de numeros Impares: ";
+    for (int i=0;i<contImpar;i++){
+        cout<<impares[i]<<" ";
+    }
+
+
+      return 0;
+    }
+
+
+  
