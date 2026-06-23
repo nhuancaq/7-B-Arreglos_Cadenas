fb69fddadf41e57a1569d167cfb60845f00b359c
nhuancaq <natalia.lizethqq@gmail.com>
2026-06-23 09:38:50 -0600

    Ejercicio 3: Registro de temperaturas semanales y calculo de medias - (Ingresar temperaturas de los 7 días)

---
+++ Ejercicio3.cpp
@@ -0,0 +1,35 @@
+//Ejercicio 3
+//Queremos guardar la temperatura mínima y máxima de los siete días de una semana. Realice un programa que de la siguiente información:
+//La temperatura media de cada día
+//El día con menor temperatura.
+//Se lee una temperatura por teclado y se muestran los días cuya temperatura máxima coincide con ella. si no existe ningún día se muestra un mensaje de información.
+
+#include <iostream>
+using namespace std;
+int main() {
+
+    float minima[7];
+    float maxima[7];
+    float media[7];
+
+    float menor;
+    int diaMenor;
+
+    cout<< "Ingresar temperaturas minimas y maximas de los 7 dias:" << endl;
+
+    for (int i = 0; i < 7; i++) {
+        cout << "Dia " << i + 1 << endl;
+
+        cout << "Temperatura minima: ";
+        cin >> minima[i];
+
+        cout << "Temperatura maxima: ";
+        cin >> maxima[i];
+
+        media[i] = (minima[i] + maxima[i]) / 2;
+    }
+
+
+  
+    return 0;
+}
